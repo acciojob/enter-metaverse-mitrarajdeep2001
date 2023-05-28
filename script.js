6504406340b67d2fc1040b2e6a0ad329819e667f
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-05-28 15:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 document.getElementById('enterBtn').addEventListener('click', () => {
-	document.getElementById('status').innerText = 'Entered Metaverse';
+	document.getElementById('status').innerHTML = '<h1>Entered Metaverse</h1>';
 	// document.querySelector('h1').style.display = 'block';
 })  
